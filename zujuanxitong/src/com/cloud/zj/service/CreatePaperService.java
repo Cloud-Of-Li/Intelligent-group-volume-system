781a2afcb965359319eb7916aae42cee7bc5d1e3
Cloud-Of-Li <1224479869@qq.com>
2018-04-11 20:09:59 +0800

    添加方法

---
+++ zujuanxitong/src/com/cloud/zj/service/CreatePaperService.java
@@ -0,0 +1,5 @@
+package com.cloud.zj.service;
+
+public class CreatePaperService {
+	
+}
---
+++ zujuanxitong/src/com/cloud/zj/servlet/CreatePaperServlet.java
@@ -0,0 +1,66 @@
+package com.cloud.zj.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/createPaperServlet")
+public class CreatePaperServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public String readJSONString(HttpServletRequest request) {
+		StringBuffer json = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while((line = reader.readLine()) != null) {
+				json.append(line);
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return json.toString();
+	}
+
+}
