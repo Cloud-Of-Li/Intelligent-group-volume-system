e952795a64c8ed155934d6e7b63a36ec447e18ec
Cloud-Of-Li <1224479869@qq.com>
2018-04-29 13:43:05 +0800

    增添daoimp

---
+++ zujuanxitong/src/com/cloud/zj/dao/imp/ManagerDaoImp.java
@@ -0,0 +1,9 @@
+package com.cloud.zj.dao.imp;
+
+import com.cloud.zj.dao.ManagerDao;
+import com.cloud.zj.entity.Manager;
+
+public class ManagerDaoImp extends BaseDaoImp<Manager> implements ManagerDao {
+
+
+}
